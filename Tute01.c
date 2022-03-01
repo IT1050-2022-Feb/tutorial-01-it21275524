2856224ce5bd5d4fc626194d491bc2f340252f4d
|NU| <100335260+it21275524@users.noreply.github.com>
2022-03-01 07:21:40 +0000

    i have completed 1 tute

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+
+  float sub1,sub2,avg;
+
+  printf("\n\nEnter marks for Subject 1 : ");
+  scanf("%f" , &sub1);
+
+  printf("\nEnter marks for Subject 2 : ");
+  scanf("%f" , &sub2);
+
+  avg = (sub1 + sub2)/2;
+
+    printf("Average is : %.2f" , avg);
   
   return 0;
 }
